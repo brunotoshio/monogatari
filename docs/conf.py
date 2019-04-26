13a28d59e022f4e63b72c35fd8f79374b7279a37
Bruno Toshio Sugano <brunotoshiosugano@Brunos-Mac-mini.local>
2019-04-26 14:54:12 +0900

    Bump version: 0.0.1 → 0.0.2

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ project = 'Monogatari'
 year = '2019'
 author = 'Bruno Toshio Sugano'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.1'
+version = release = '0.0.2'
 
 pygments_style = 'trac'
 templates_path = ['.']
