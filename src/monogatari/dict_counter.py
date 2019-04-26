8ad9d2459426454f68b3bbfe833a5df058d5b85e
Bruno Toshio Sugano <brunotoshiosugano@Brunos-Mac-mini.local>
2019-04-26 16:07:36 +0900

    check

--- src/monogatari/dict_counter.py
+++ src/monogatari/dict_counter.py
@@ -9,20 +9,20 @@ class DictCounter(object):
     def __init__(self, dict_path):
         self._dict = DictHandler(dict_path)
         self.reset()
-    
+
     def count(self, list_of_words):
         for word in list_of_words:
             self.number_of_words += 1
             self._category_counter.update(self._dict.search(word))
-    
+
     def reset(self):
         self.number_of_words = 0
         self._category_counter = Counter()
-        self._category_counter.update({category:0 for category in self._dict.categories()})
+        self._category_counter.update({category: 0 for category in self._dict.categories()})
 
     def top(self, n):
         return self._category_counter.most_common(n)
-    
+
     def top_normalized(self, n):
         normalized = Counter()
         for key in self._category_counter:
