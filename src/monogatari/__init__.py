d65da76b15ab560d0fcb5f896c1b5ed6abbf8242
Bruno Toshio Sugano <brunotoshio@gmail.com>
2023-10-07 14:46:45 +0900

    Bump version: 2.1.0 → 2.1.1

--- src/monogatari/__init__.py
+++ src/monogatari/__init__.py
@@ -1,7 +1,7 @@
 """Handles counting of words using dictionaries
 """
 
-__version__ = "2.1.0"
+__version__ = "2.1.1"
 
 from .dict_counter import DictCounter  # NOQA
 from .jmfd_counter import JMFDCounter  # NOQA
